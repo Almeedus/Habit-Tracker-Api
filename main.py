7f6cce3e63d652fbe0535e56ef40f3153e63a5fb
Eduardo Almeida <almeedusa@gmail.com>
2026-01-21 11:00:04 -0300

    feat: list habits using in-memory storage

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ class HabitCreate(BaseModel):
     name: str
     frequency: str
 
+habits:HabitCreate = []
+
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
 @app.post("/habits")
 def create_habits(habit: HabitCreate):
+    habits.append(habit)
     return {
         "message": "Habit created successfully",
         "habit": habit
     }
+    
+
+@app.get("/habits")
+def list_habits() -> list[HabitCreate]:
+    return habits
